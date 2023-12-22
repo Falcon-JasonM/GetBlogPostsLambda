3082d3fb46c27d492c374d08a1bfb535510601af
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2023-12-21 23:33:37 -0600

    "very basic search and pagination"

--- src/main/java/handlers/RetrieveBlogPostsLambda.java
+++ src/main/java/handlers/RetrieveBlogPostsLambda.java
@@ -6,20 +6,17 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import software.amazon.awssdk.thirdparty.jackson.core.JsonProcessingException;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -27,13 +24,12 @@ import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class RetrieveBlogPostsLambda implements RequestStreamHandler {
 
     private static final String DB_URL = System.getenv("DB_URL_KEY");
 
     @Override
-    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+    public void handleRequest(InputStream input, OutputStream output, Context context) {
         LambdaLogger LOGGER = context.getLogger();
         LOGGER.log("Starting RetrieveBlogPostsLambda\n", LogLevel.INFO);
         JSONParser parser = new JSONParser();
@@ -50,15 +46,33 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
 
         try {
             // Parse the received JSON content into an APIGatewayV2HTTPEvent object
-            JSONObject event = (JSONObject) parser.parse(new InputStreamReader(input));
-            requestEvent = mapper.readValue(event.toJSONString(), APIGatewayV2HTTPEvent.class);
-            LOGGER.log("Parsed APIGatewayV2HTTPEvent: " + requestEvent);
+            JSONObject event;
+            try {
+                event = (JSONObject) parser.parse(new InputStreamReader(input));
+            } catch (IOException | ParseException ex) {
+                LOGGER.log("Error parsing input into JSON format: " + ex.getMessage(), LogLevel.ERROR);
+                throw new RuntimeException(ex);
+            }
+            try {
+                requestEvent = mapper.readValue(event.toJSONString(), APIGatewayV2HTTPEvent.class);
+            } catch (JsonProcessingException ex) {
+                LOGGER.log("Error parsing APIGatewayV2HTTPEvent: " + ex.getMessage(), LogLevel.ERROR);
+                throw new RuntimeException(ex);
+            }
+            LOGGER.log("Parsed APIGatewayV2HTTPEvent: " + requestEvent, LogLevel.DEBUG);
+
             // Return http status = 200 OK if request method is OPTIONS
-            if (requestEvent.getHeaders() != null && requestEvent.getHeaders().containsKey("httpMethod") && "OPTIONS".equals(requestEvent.getHeaders().get("httpMethod"))) {
+            if (requestEvent.getHeaders() != null && requestEvent.getHeaders().containsKey("httpMethod") &&
+                    "OPTIONS".equals(requestEvent.getHeaders().get("httpMethod"))) {
                 responseEvent.setStatusCode(200);
                 responseEvent.setHeaders(headers);
                 responseEvent.setIsBase64Encoded(false);
-                mapper.writeValue(output, responseEvent);
+                try {
+                    mapper.writeValue(output, responseEvent);
+                } catch (IOException ex) {
+                    LOGGER.log("Error processing pre-flight options request: " + ex.getMessage(), LogLevel.ERROR);
+                    throw new RuntimeException(ex);
+                }
                 return;
             }
 
@@ -82,7 +96,6 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                 LOGGER.log("Got DB credentials\n", LogLevel.DEBUG);
 
                 LOGGER.log("Attempting to connect to the DB...\n", LogLevel.INFO);
-
                 // Load the PostgreSQL JDBC driver
                 Class.forName("org.postgresql.Driver");
                 connection = DriverManager.getConnection(DB_URL, dbUsername, dbPassword);
@@ -94,41 +107,29 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                 // Check for query parameters and perform search if present
                 if (requestEvent.getQueryStringParameters() != null) {
                     try {
-                        LOGGER.log("Query string parameters: " + requestEvent.getQueryStringParameters() + "\n", LogLevel.DEBUG);
-                        Map<String, String> queryStringParameters;
-                        queryStringParameters = requestEvent.getQueryStringParameters();
+                        LOGGER.log("Query string parameters: " + requestEvent.getQueryStringParameters() + "\n");
+
+                        Map<String, String> queryStringParameters = requestEvent.getQueryStringParameters();
                         String searchTerm = queryStringParameters.getOrDefault("searchTerm", null);
                         String page = queryStringParameters.getOrDefault("page", "1");
                         String limit = queryStringParameters.getOrDefault("limit", "5");
-                        // String sort = queryStringParameters.getOrDefault("sort", null);
-                        String order = queryStringParameters.getOrDefault("order", null);
-                        // String searchField = queryStringParameters.getOrDefault("searchField", "content");
-                        // String searchType = queryStringParameters.getOrDefault("searchType", null);
-                        // String searchCategory = queryStringParameters.getOrDefault("searchCategory", null);
+                        String order = queryStringParameters.getOrDefault("order", "d");
                         String searchTags = queryStringParameters.getOrDefault("searchTags", null);
                         String searchKeywords = queryStringParameters.getOrDefault("searchKeywords", null);
 
                         // Generate PostgreSQL query based on query parameters
-                        StringBuilder query = new StringBuilder("SELECT * FROM blog_page.blog_post");
+                        StringBuilder query = new StringBuilder("SELECT * FROM blog_page.blog_post WHERE 1 = 1");
 
                         if (searchTerm != null) {
-                            query.append(" WHERE content ILIKE ?");
+                            searchTerm = searchTerm.replace(" ", "%");
+                            query.append(" AND content ILIKE ?");
                         }
-//                      if (searchField != null) {
-//                        query.append(" AND ").append(searchField).append(" ");
-//                      }
-//                      if (searchCategory != null) {
-//                        query.append(" AND category = ?");
-//                      }
                         if (searchTags != null) {
                             query.append(" AND tags @> ARRAY[?]::text[]");
                         }
                         if (searchKeywords != null) {
                             query.append(" AND keywords @> ARRAY[?]::text[]");
                         }
-//                      if (sort != null) {
-//                        query.append(" ORDER BY ?");
-//                      }
                         if (order != null) {
                             query.append(" ORDER BY ?");
                         }
@@ -139,7 +140,7 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                             query.append(" OFFSET ?");
                         }
 
-                        LOGGER.log("Generated query: " + query + "\n", LogLevel.DEBUG);
+                        LOGGER.log("Generated query: " + query + "\n");
 
                         // Prepare and execute the query
                         PreparedStatement preparedStatement = connection.prepareStatement(query.toString());
@@ -148,38 +149,33 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                         if (searchTerm != null) {
                             preparedStatement.setString(parameterIndex++, "%" + searchTerm + "%");
                         }
-//                      if (searchField != null) {
-//                        preparedStatement.setString(parameterIndex++, searchTerm);
-//                      }
-//                      if (searchCategory != null) {
-//                        preparedStatement.setString(parameterIndex++, searchCategory);
-//                      }
                         if (searchTags != null) {
                             preparedStatement.setString(parameterIndex++, searchTags);
                         }
                         if (searchKeywords != null) {
                             preparedStatement.setString(parameterIndex++, searchKeywords);
                         }
-//                      if (sort != null) {
-//                        preparedStatement.setString(parameterIndex++, sort);
-//                      }
                         if (order != null) {
+                            if ("a".equals(order)) {
+                                order = "ASC";
+                            } else {
+                                order = "DESC";
+                            }
                             preparedStatement.setString(parameterIndex++, order);
                         }
                         if (limit != null) {
                             preparedStatement.setInt(parameterIndex++, Integer.parseInt(limit));
                         }
                         if (page != null) {
-                            preparedStatement.setInt(parameterIndex++, (Integer.parseInt(page) - 1) * Integer.parseInt(limit));
+                            preparedStatement.setInt(parameterIndex, (Integer.parseInt(page) - 1) * Integer.parseInt(limit));
                         }
-
+                        LOGGER.log("Prepared statement: " + preparedStatement + "\n", LogLevel.DEBUG);
                         resultSet = preparedStatement.executeQuery();
 
                     } catch (Exception e) {
-                        LOGGER.log("Exception: " + e.getMessage(), LogLevel.ERROR);
+                        LOGGER.log("Exception: " + e.getMessage());
                         // Prepare error response
                         String errorResponse = "{\"error\": \"An error occurred while processing the search request.\"}";
-                        responseEvent = new APIGatewayV2HTTPResponse();
                         responseEvent.setStatusCode(500); // Internal Server Error
                         responseEvent.setHeaders(headers);
                         responseEvent.setBody(errorResponse);
@@ -187,7 +183,8 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                         return;
                     }
                 } else {
-                    // Fetch last 5 blog posts from the DB
+                    // Fetch last 5 blog posts from the DB based on ID
+                    LOGGER.log("No query parameters found\n", LogLevel.DEBUG);
                     Statement statement = connection.createStatement();
                     resultSet = statement.executeQuery("SELECT * FROM blog_page.blog_post ORDER BY id DESC LIMIT 5;");
                 }
@@ -203,14 +200,14 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                         // Create the JSON object for the post
                         ObjectNode postNode = mapper.createObjectNode();
                         postNode.put("title", postTitle);
-                        postNode.put("content", postContent); // Store the raw HTML content
+                        postNode.put("content", postContent);
 
                         blogPostsArray.add(postNode);
                     }
                 } else {
                     ObjectNode postNode = mapper.createObjectNode();
                     postNode.put("title", "No Results Found");
-                    postNode.put("content", "Please rephrase your search term and try again."); // Store the raw HTML content
+                    postNode.put("content", "Please rephrase your search term and try again.");
 
                     blogPostsArray.add(postNode);
                 }
@@ -252,8 +249,8 @@ public class RetrieveBlogPostsLambda implements RequestStreamHandler {
                 LOGGER.log("Error writing response: " + e.getMessage(), LogLevel.ERROR);
 
             }
-        } catch (
-                ParseException e) {
+        } catch ( IOException e) {
+            LOGGER.log("Error reading input: " + e.getMessage(), LogLevel.ERROR);
             throw new RuntimeException(e);
         }
     }
